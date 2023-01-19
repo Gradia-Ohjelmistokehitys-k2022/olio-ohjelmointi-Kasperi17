21be6306c7af13ac2d79f11e5fcae422691895ea
Kasperi Salonen <AD4449@student.jamk.fi>
2023-01-19 13:41:47 +0200

    Tehtävä valmis

---
+++ 05_Velka/05_Velka/05_Velka/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Velka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Velka raha = new Velka(100, 0.05);
+            raha.TulostaSaldo();
+            raha.OdotaVuosi();
+            raha.TulostaSaldo();
+            
+
+            
+        }
+    }
+}
---
+++ 05_Velka/05_Velka/05_Velka/Velka.cs
@@ -0,0 +1,28 @@
+﻿namespace Velka
+{
+    internal class Velka
+    {
+        double _saldo, _korkoProsentti;
+        
+
+        public Velka(double saldoAlussa, double korkoProsentti)
+        {
+            this._saldo = saldoAlussa;
+            this._korkoProsentti= korkoProsentti;
+        }
+
+        public void TulostaSaldo()
+        {
+            Console.WriteLine(_saldo);
+            Console.WriteLine(_korkoProsentti);
+            
+        }
+        public void OdotaVuosi()
+        {
+            Console.WriteLine((_saldo = _saldo * (1 + _korkoProsentti)));
+        }
+    }
+
+
+
+}
