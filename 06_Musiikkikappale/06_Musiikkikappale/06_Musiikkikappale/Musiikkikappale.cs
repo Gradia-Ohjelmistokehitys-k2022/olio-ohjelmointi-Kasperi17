ce88b1b154e8da81553e16f27f0453b2f2fb6819
Kasperi Salonen <AD4449@student.jamk.fi>
2023-01-20 09:08:05 +0200

    Tehtävä valmis

---
+++ 06_Musiikkikappale/06_Musiikkikappale/06_Musiikkikappale/Musiikkikappale.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Musiikkikappale
+{
+    internal class Musiikkikappale
+    {
+        string nimi;
+        int pituus;
+
+
+        public Musiikkikappale(string kappaleenNimi, int kappaleenPituus)
+        {
+            this.nimi = kappaleenNimi;
+            this.pituus = kappaleenPituus;
+            
+        }
+        
+        public string Nimi()
+        {
+            return nimi;
+        }
+        public int Pituus()
+        {   
+
+            return pituus;
+        }
+
+    }
+        
+}
---
+++ 06_Musiikkikappale/06_Musiikkikappale/06_Musiikkikappale/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Musiikkikappale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musiikkikappale garden = new Musiikkikappale("HeiMoiTervehdys", 10910);
+            Console.WriteLine("Kappaleen " + garden.Nimi() + " pituus on " + garden.Pituus() + " sekuntia.");
+
+        }
+    }
+}
