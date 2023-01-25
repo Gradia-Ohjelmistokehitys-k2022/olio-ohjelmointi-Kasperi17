30f40719bec51b2dbecbd81bc17f3d264ac89dab
Kasperi Salonen <AD4449@student.jamk.fi>
2023-01-25 08:58:52 +0200

    Tehtävä valmis

---
+++ 08_Maksukortti/01_Maksukortti/01_Maksukortti/Maksukortti.cs
@@ -0,0 +1,58 @@
+﻿namespace Maksukortti
+{
+    public class Maksukortti
+    {
+        private double saldo;
+
+        public Maksukortti(double alkusaldo)
+        {
+            saldo = alkusaldo;
+        }
+
+        public override String ToString()
+        {
+            return "kortilla on rahaa " + saldo + " euroa";
+        }
+
+        public void SyoEdullisesti()
+        {
+            if (saldo >= 2.60)
+            {
+                saldo = saldo - 2.60;
+            }
+
+        }
+
+        public void SyoMaukkaasti()
+        {
+            if (saldo >= 2.60)
+            {
+                saldo = saldo - 4.60;
+            }
+
+        }
+        public void LataaRahaa(double lisaa)
+        {
+            if (lisaa < 0)
+            {
+                lisaa = 0;
+            }
+            saldo = saldo + lisaa;
+            
+            
+        }
+        
+        
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
---
+++ 08_Maksukortti/01_Maksukortti/01_Maksukortti/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Maksukortti
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Maksukortti pekanKortti = new Maksukortti(20);
+            Maksukortti matinKortti = new Maksukortti(30);
+            pekanKortti.SyoEdullisesti();
+            matinKortti.SyoMaukkaasti();
+            Console.WriteLine(pekanKortti);
+            Console.WriteLine(matinKortti);
+            pekanKortti.LataaRahaa(20);
+            matinKortti.SyoMaukkaasti();
+            Console.WriteLine(pekanKortti);
+            Console.WriteLine(matinKortti);
+            pekanKortti.SyoEdullisesti();
+            pekanKortti.SyoEdullisesti();
+            matinKortti.LataaRahaa(50);
+            Console.WriteLine(pekanKortti);
+            Console.WriteLine(matinKortti);
+        }
+    }
+}
