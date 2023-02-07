7c6793d19907498c4267214403489d518f0ad4a8
Kasperi Salonen <AD4449@student.jamk.fi>
2023-02-07 09:11:20 +0200

    Tehtävä

---
+++ 09_Luokkakirjastot ja periytyminen/Luokkakirjastot ja periytyminen 1/Ika/Ika/Program.cs
@@ -0,0 +1,18 @@
+﻿using Elain_Kirjasto;
+
+for (int i = 0; i < 10; i++)
+{
+    kissa cat = new kissa();
+    koira dog = new koira();
+    Console.WriteLine("Koira on: " + dog.PalautaIka() + " vuotta, Kissa on: " + cat.PalautaIka() + " vuotta");
+    if (cat.PalautaIka() > dog.PalautaIka())
+    {
+        Console.WriteLine("Kissa on vanhmepi");
+    }
+    else
+    {
+        Console.WriteLine("koira on vanhempi");
+    }
+    dog.Haukku();
+    cat.Kehraa();
+}
